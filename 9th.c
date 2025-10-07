192bc13512080a019c88f91c304eef94d5eb4486
SnehaSingh2504 <snehasingh.mst20@gmail.com>
2025-10-07 12:33:54 +0530

    Update 9th.c

--- 9th.c
+++ 9th.c
@@ -1,32 +1,42 @@
 #include <stdio.h>
-int n, m;
-int arr[100][100];
-int visited[100][100];
-int dx[4] = {0, 0, 1, -1};
-int dy[4] = {1, -1, 0, 0};
-int dfs(int x, int y) {
-  if(x < 0 || x >= n || y < 0 || y >= m || arr[x][y] == 1 || visited[x][y])
-        return 0;
-    if(x == n-1 && y == m-1)
-        return 1;
-    visited[x][y] = 1;
-    for(int i=0; i<4; i++) {
-     if(dfs(x + dx[i], y + dy[i]))
-      return 1; 
-    }
-    return 0; 
-}
 
 int main() {
- scanf("%d %d", &n, &m);
-  for(int i=0; i<n; i++)
-    for(int j=0; j<m; j++)
- scanf("%d", &arr[i][j]);
-  for(int i=0; i<n; i++)
-        for(int j=0; j<m; j++)
-            visited[i][j] = 0;
+    int n, m;
+    scanf("%d %d", &n, &m);
+    int arr[10][10];     
+    int visited[10][10] = {0};
+    for(int i=0;i<n;i++)
+        for(int j=0;j<m;j++)
+            scanf("%d", &arr[i][j]);
+    visited[0][0] = 1;
+    int changed = 1;
+    while(changed) {
+        changed = 0;
+        for(int i=0;i<n;i++) {
+            for(int j=0;j<m;j++) {
+                if(visited[i][j]) {
+                    if(j+1 < m && arr[i][j+1]==0 && !visited[i][j+1]) {
+                        visited[i][j+1] = 1;
+                        changed = 1;
+                    }
+                    if(i+1 < n && arr[i+1][j]==0 && !visited[i+1][j]) {
+                        visited[i+1][j] = 1;
+                        changed = 1;
+                    }
+                    if(j-1 >=0 && arr[i][j-1]==0 && !visited[i][j-1]) {
+                        visited[i][j-1] = 1;
+                        changed = 1;
+                    }
+                    if(i-1 >=0 && arr[i-1][j]==0 && !visited[i-1][j]) {
+                        visited[i-1][j] = 1;
+                        changed = 1;
+                    }
+                }
+            }
+        }
+    }
 
-    if(dfs(0,0))
+    if(visited[n-1][m-1])
         printf("true\n");
     else
         printf("false\n");
